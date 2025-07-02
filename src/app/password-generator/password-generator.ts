2699bcee4b48ec1ac947851a0d1f40d5af908ff3
orrest <zzhxufeng@163.com>
2025-07-02 08:45:04 +0800

    active 1/n

--- src/app/password-generator/password-generator.ts
+++ src/app/password-generator/password-generator.ts
@@ -1,17 +1,41 @@
-import { Component } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+
+interface Rule {
+  description: string;
+  checked: boolean;
+}
 
 @Component({
   selector: 'app-password-generator',
-  imports: [NgOptimizedImage],
+  imports: [],
   templateUrl: './password-generator.html',
   styleUrl: './password-generator.css',
 })
 export class PasswordGenerator {
-  rules = [
-    'Include Uppercase Letters',
-    'Include Uppercase Letters',
-    'Include Uppercase Letters',
-    'Include Uppercase Letters',
-  ];
+  rules = signal<Rule[]>([
+    { description: 'Include Uppercase Letters', checked: false },
+    { description: 'Include Lowercase Letters', checked: false },
+    { description: 'Include Numbers', checked: false },
+    { description: 'Include Symbols', checked: false },
+  ]);
+
+  checkedRules = computed(() => {
+    const result = this.rules().filter((r) => r.checked);
+    console.log('checkedRules ' + result);
+    return result;
+  });
+
+  uncheckedRules = computed(() => {
+    return this.rules().filter((r) => !r.checked);
+  });
+
+  onCheckChange(event: Event, index: number) {
+    const checkInput = event.target as HTMLInputElement;
+
+    this.rules.update((rules) => {
+      return rules.map((rule, i) =>
+        i === index ? { ...rule, checked: checkInput.checked } : rule,
+      );
+    });
+  }
 }
